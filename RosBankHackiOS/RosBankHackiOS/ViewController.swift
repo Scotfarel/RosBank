50de50e65bcccb14aa2cbe89a6336bc4dcae72b9
Ivan Zakharov <scotfarel@gmail.com>
2019-12-08 02:33:50 +0300

    mobile iOS app

---
+++ RosBankHackiOS/RosBankHackiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RosBankHackiOS
+//
+//  Created by Артём Ануфриев on 06.12.2019.
+//  Copyright © 2019 Артём Ануфриев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
